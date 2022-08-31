f23b73745bd3be931395b27489a71d5546001ac6
Goodnews Daniel <goodnewsdaniel101@gmail.com>
2022-08-31 19:02:05 +0100

    Edited and updated index.html

--- assets/js/service-request.js
+++ assets/js/service-request.js
@@ -1,34 +1,37 @@
+try{
 window.onload = function(){
-
-  document.getElementById("ServiceForm").addEventListener("submit", function (event) {
-    event.preventDefault();
-    //IDs
-    const serviceID = "service_3mah583",
-      templateID = "template_l7n12vg";
-    let success = document.getElementById("service_email_success"),
-      error = document.getElementById("service_email_error");
-
-    emailjs.sendForm(serviceID, templateID, this).then(
-      function () {
-        success.classList.remove("d-none");
-        success.style.color = "SeaGreen";
-        setTimeout(() => {
-          success.classList.add("d-none");
-        }, 2500);
-        clearForm();
-      },
-      function () {
-        error.classList.remove("d-none");
-        error.style.color = "Red";
-        setTimeout(() => {
-          error.classList.add("d-none");
-        }, 2500);
-        clearForm();
-      }
-    );
-  });
+    document.getElementById("ServiceForm").addEventListener("submit", function (event) {
+      event.preventDefault();
+      //IDs
+  
+      const serviceID = "service_3mah583",
+        templateID = "template_l7n12vg";
+      let success = document.getElementById("service_email_success"),
+        error = document.getElementById("service_email_error");
+  
+      emailjs.sendForm(serviceID, templateID, this).then(
+        function () {
+          success.classList.remove("d-none");
+          success.style.color = "SeaGreen";
+          setTimeout(() => {
+            success.classList.add("d-none");
+          }, 2500);
+          clearForm();
+        },
+        function () {
+          error.classList.remove("d-none");
+          error.style.color = "Red";
+          setTimeout(() => {
+            error.classList.add("d-none");
+          }, 2500);
+          clearForm();
+        }
+      );
+    });  
 }
-
+} catch (error) {
+  alert("Server not allowed." + error)
+}
 // Clear Forms
 function clearForm() {
   const name = document.getElementById("s_name"),
